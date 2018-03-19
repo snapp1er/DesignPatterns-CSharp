fb941d38f5f63a5633f735eeec37e4c83c6c97dc
snappy <asdasdasdf@gmail.com>
2018-03-19 11:54:41 +0300

    Added output comments to Visitor

--- DesignPatterns/Behavioral/Visitor/Shapes/Shapes.cs
+++ DesignPatterns/Behavioral/Visitor/Shapes/Shapes.cs
@@ -12,13 +12,14 @@ namespace DesignPatterns.Behavioral.Visitor.Shapes
             var circle = new Circle(10);
             
             Console.WriteLine($"Circle area: {circle.Accept(new AreaCalculator())}");
+            /* Circle area: 314,1593 */
 
             var areaMeter = new AreaCalculator();
             float AreaOfSeveralSquares = Enumerable.Range(1, 3)
                                 .Select(i => new Square(i))
                                 .Sum(c => c.Accept(areaMeter));
             Console.WriteLine($"total area: {AreaOfSeveralSquares}");
-
+            /* total area: 14 */
 
             
         }
