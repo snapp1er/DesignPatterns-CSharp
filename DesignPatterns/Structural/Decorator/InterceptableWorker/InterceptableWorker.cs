75df24051ac4f10b551e093465f6b9ce7f61c6fc
snappy <asdasdasdf@gmail.com>
2018-03-22 12:58:50 +0300

    Added Decorator pattern to Structural patterns

---
+++ DesignPatterns/Structural/Decorator/InterceptableWorker/InterceptableWorker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Structural.Decorator.InterceptableWorker
+{
+    interface IWorker
+    {
+        void DoThis();
+    }
+
+    class Worker : IWorker
+    {
+        public void DoThis()
+        {
+            Console.WriteLine("Doing work...");
+        }
+    }
+
+    class InterceptableWorker : IWorker
+    {       
+        public event Action BeforeWorkStarted;
+        public event Action AfterWorkFinished;
+
+        IWorker inner;
+        public InterceptableWorker(IWorker inner)
+        {
+            this.inner = inner;
+        }
+
+        public static void Main(string[] args)
+        {
+            var interceptableWorker = new InterceptableWorker(new Worker());
+            interceptableWorker.BeforeWorkStarted += () => { Console.WriteLine("Do this before start"); };
+            //interceptableWorker.AfterWorkFinished += () => { Console.WriteLine("Do this after finish"); };
+            interceptableWorker.DoThis();
+        }
+
+        
+        public new void DoThis()
+        {
+            BeforeWorkStarted?.Invoke();
+            inner.DoThis();
+            AfterWorkFinished?.Invoke();
+        }
+    }
+}
