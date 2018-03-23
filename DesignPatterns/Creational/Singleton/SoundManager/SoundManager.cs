a3057757cbf1340b0984c01fee1a62eb5ffe456f
snappy <asdasdasdf@gmail.com>
2018-03-23 12:45:49 +0300

    Added Singleton pattern to Creational patterns ( SoundManager)

---
+++ DesignPatterns/Creational/Singleton/SoundManager/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Creational.Singleton.SoundManager
+{
+    class SoundManager
+    {
+        private static SoundManager instance;
+        public static SoundManager Instance
+        {
+            get { return instance; }
+        }
+        public static void Init()
+        {
+            if(instance == null)
+            {
+                instance = new SoundManager();
+            }
+        }
+        private SoundManager()
+        {
+        }
+        public void PlaySound(int n)
+        {
+            Console.WriteLine("Playing sound : {0}", n);
+        }
+        public static void Main(string[] args)
+        {
+            SoundManager.Init();
+            SoundManager.Instance.PlaySound(3);
+            SoundManager.Instance.PlaySound(15);
+            /*
+            Playing sound : 3
+            Playing sound : 15
+            */
+        }
+    }
+}
